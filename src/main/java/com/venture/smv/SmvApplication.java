b68b3e425d25ae7820d59de00bacfbd1b05628a3
Utsav Tripathi <utsavtripathi@Utsavs-MacBook-Pro.local>
2024-02-15 22:53:33 +0530

    first commit

---
+++ src/main/java/com/venture/smv/SmvApplication.java
@@ -0,0 +1,13 @@
+package com.venture.smv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/venture/smv/SmvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venture.smv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
